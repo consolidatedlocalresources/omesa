69072acfab12b36bf389f81eb3b4393835a27c55
Jim Peterson <jamespeterson137@gmail.com>
2019-06-10 13:08:02 -0400

    Added post css and a few postcss tools to gulp.js and also added a pipe to convert our styles.css file into a new file destination for the original piped file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,35 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
+// Test function
 gulp.task('default', function() {
     console.log('Hooray, you created a Gulp task!');
 });
 
+function html() {
+    console.log('HTML modified!');
+};
+
+function styles() {
+    // Define source file
+    return gulp.src('./app/assets/styles/styles.css')
+    // Define mods to source files
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    // Define destination file to be created out of modified source files
+    .pipe(gulp.dest('./app/temp/styles'));
+}
+
 // Watch for changes within certain files
 gulp.task('watch', function() {
+    
     // Watch for HTML mods
-    watch('./app/index.html', function html() {
-        // Result of index.html mods
-        console.log('Imagine something useful being done to your HTML!');
-    });
+    watch('./app/index.html', html);
+
+    // Watch for CSS mods
+    watch('./app/assets/styles/**/*.css', styles);
+
 });
