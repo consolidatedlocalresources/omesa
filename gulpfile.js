d1d65813dc9dd912d8289672324a282b47280c1a
Jim Peterson <jamespeterson137@gmail.com>
2019-06-11 06:58:05 -0400

    Implemented normalize.css, completed added BEM, and got css architecture completely set up

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 // Test function
 gulp.task('default', function() {
@@ -18,7 +19,7 @@ function styles() {
     // Define source file
     return gulp.src('./app/assets/styles/styles.css')
     // Define mods to source files
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     // Define destination file to be created out of modified source files
     .pipe(gulp.dest('./app/temp/styles'));
 }
